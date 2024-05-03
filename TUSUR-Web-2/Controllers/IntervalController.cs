7299d1687232653f76d4cc43afacde105c96a7a0
TRDC <vudi600@gmail.com>
2024-05-03 19:51:11 +0700

    Добавьте файлы проекта.

---
+++ TUSUR-Web-2/Controllers/IntervalController.cs
@@ -0,0 +1,23 @@
+﻿using TUSUR_Web_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TUSUR_Web_2.Controllers
+{
+    public class IntervalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Analyze(string numbers)
+        {
+            int[] interval = numbers.Trim().Split(' ').Select(int.Parse).ToArray();
+            var result = IntervalModel.InInterval(interval, -5, 6);
+            ViewBag.Interval = string.Join(" ", result);
+            return View("Index");
+        }
+    }
+
+}
---
+++ TUSUR-Web-2/Views/Odd/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TUSUR_Web_2.Views.Odd
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
